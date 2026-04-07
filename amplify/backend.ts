270cebbd3276d590074a05d78006e6d9798d8d97
QFIND Project <cto@qfind.io>
2026-04-07 10:20:42 +0300

    Update backend.ts
    Delete function Lambda

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -2,7 +2,6 @@ import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
 import { storage } from './storage/resource';
-import { presignedUrlFunction } from './functions/presigned-url/resource';
 
 /**
  * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
@@ -10,13 +9,5 @@ import { presignedUrlFunction } from './functions/presigned-url/resource';
 defineBackend({
   auth,
   data,
-  storage,
-  presignedUrlFunction
+  storage
 });
-
-// Grant the Lambda read access to the S3 bucket
-const s3Bucket = backend.storage.resources.bucket;
-const lambdaFn = backend.PresignedUrlFunction.resources.lambda;
-
-s3Bucket.grantRead(lambdaFn);
-lambdaFn.addEnvironment("S3_BUCKET_NAME", s3Bucket.bucketName);
