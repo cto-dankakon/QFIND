b1711e541bd14ff703c34457c35a1e7931cf45c1
QFIND Project <cto@qfind.io>
2026-04-07 11:40:05 +0300

    Update backend.ts
    Add Tags for S3 bucket

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -2,6 +2,7 @@ import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
 import { storage } from './storage/resource';
+import { Tags } from 'aws-cdk-lib';
 
 /**
  * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
@@ -11,3 +12,12 @@ defineBackend({
   data,
   storage
 });
+
+// Access the underlying S3 bucket and add tags
+const s3Bucket = backend.storage.resources.bucket;
+Tags.of(s3Bucket).add('cost:business-unit', 'qfind');
+Tags.of(s3Bucket).add('cost:application', 'qfind mobile application');
+Tags.of(s3Bucket).add('cost:component', 'bucket');
+Tags.of(s3Bucket).add('cost:environment', 'dev');
+Tags.of(s3Bucket).add('managed:by', 'cto');
+Tags.of(s3Bucket).add('owner', 'cto');
